41a8fb309be2458afa110c129ef3471e77bcb726
Shadou1 <shadou777@gmail.com>
2025-12-08 18:02:47 +0700

    Add day 7: part 2

--- 07-laboratories/src/part2.rs
+++ 07-laboratories/src/part2.rs
@@ -1,5 +1,21 @@
-pub fn solution(input: &str) {
-    todo!()
+pub fn solution(input: &str) -> u64 {
+    let mut lines = input.lines().map(|line| line.as_bytes());
+    let first_line = lines.next().unwrap();
+    let start = first_line.iter().position(|byte| *byte == b'S').unwrap();
+    let mut beams = vec![0; first_line.len()];
+    beams[start] = 1;
+
+    for line in lines {
+        for (i, &space) in line.iter().enumerate() {
+            if space == b'^' && beams[i] > 0 {
+                beams[i - 1] += beams[i];
+                beams[i + 1] += beams[i];
+                beams[i] = 0;
+            }
+        }
+    }
+
+    beams.iter().sum()
 }
 
 #[cfg(test)]
