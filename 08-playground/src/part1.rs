882d7b4e448c072148d1827c874599f3dc3c9164
Shadou1 <shadou777@gmail.com>
2025-12-08 23:03:28 +0700

    Day 8: optimize

--- 08-playground/src/part1.rs
+++ 08-playground/src/part1.rs
@@ -14,12 +14,13 @@ pub fn solution(input: &str, connections: usize) -> u64 {
 
     distances.sort_by(|d1, d2| d1.1.total_cmp(&d2.1));
 
-    let mut circuits: Vec<HashSet<usize>> = vec![HashSet::new()];
-    circuits[0].insert(distances[0].0.0);
-    circuits[0].insert(distances[0].0.1);
+    let mut circuits: Vec<Vec<usize>> = vec![Vec::with_capacity(10)];
+    circuits[0].push(distances[0].0.0);
+    circuits[0].push(distances[0].0.1);
     let mut box_to_circuit: HashMap<usize, usize> = HashMap::with_capacity(1000);
     box_to_circuit.insert(distances[0].0.0, 0);
     box_to_circuit.insert(distances[0].0.1, 0);
+
     for &((box1_i, box2_i), distance) in &distances[1..connections] {
         // print!("\nBoxes {box1_i} : {box2_i}\t");
         let circuit1_i = box_to_circuit.get(&box1_i);
@@ -28,9 +29,7 @@ pub fn solution(input: &str, connections: usize) -> u64 {
         match (circuit1_i, circuit2_i) {
             (None, None) => {
                 // both boxes not part of some circuit
-                let mut new_circuit = HashSet::new();
-                new_circuit.insert(box1_i);
-                new_circuit.insert(box2_i);
+                let new_circuit = vec![box1_i, box2_i];
                 // println!("New:\t{:?}", new_circuit);
                 circuits.push(new_circuit);
                 box_to_circuit.insert(box1_i, circuits.len() - 1);
@@ -38,28 +37,28 @@ pub fn solution(input: &str, connections: usize) -> u64 {
             }
             (Some(&circuit1_i), None) => {
                 // first box already in circuit
-                circuits[circuit1_i].insert(box2_i);
+                circuits[circuit1_i].push(box2_i);
                 box_to_circuit.insert(box2_i, circuit1_i);
                 // println!("Add second:\t{:?}", circuits[circuit1_i]);
             }
             (None, Some(&circuit2_i)) => {
                 // second box already in circuit
-                circuits[circuit2_i].insert(box1_i);
+                circuits[circuit2_i].push(box1_i);
                 box_to_circuit.insert(box1_i, circuit2_i);
                 // println!("Add first:\t{:?}", circuits[circuit2_i]);
             }
             (Some(&circuit1_i), Some(&circuit2_i)) if circuit1_i != circuit2_i => {
                 // both boxes part of different circuits
                 // merge circuits
-                let mut merged_circuit: HashSet<usize> =
-                    HashSet::with_capacity(circuits[circuit1_i].len() + circuits[circuit2_i].len());
+                let mut merged_circuit: Vec<usize> =
+                    Vec::with_capacity(circuits[circuit1_i].len() + circuits[circuit2_i].len());
                 for box1_i in &circuits[circuit1_i] {
                     box_to_circuit.insert(*box1_i, circuits.len());
-                    merged_circuit.insert(*box1_i);
+                    merged_circuit.push(*box1_i);
                 }
                 for box2_i in &circuits[circuit2_i] {
                     box_to_circuit.insert(*box2_i, circuits.len());
-                    merged_circuit.insert(*box2_i);
+                    merged_circuit.push(*box2_i);
                 }
                 circuits[circuit1_i].clear();
                 circuits[circuit2_i].clear();
--- 08-playground/src/part2.rs
+++ 08-playground/src/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::parse_junction_boxes;
 
@@ -14,60 +14,66 @@ pub fn solution(input: &str) -> u64 {
 
     distances.sort_by(|d1, d2| d1.1.total_cmp(&d2.1));
 
-    let mut circuits: Vec<HashSet<usize>> = vec![HashSet::new()];
-    circuits[0].insert(distances[0].0.0);
-    circuits[0].insert(distances[0].0.1);
-    for ((box1_i, box2_i), distance) in &distances[1..] {
-        let mut circuit1_i: Option<usize> = None;
-        let mut circuit2_i: Option<usize> = None;
-        for (circuit_i, circuit) in circuits.iter().enumerate() {
-            if circuit.contains(box1_i) {
-                circuit1_i = Some(circuit_i);
-            } else if circuit.contains(box2_i) {
-                circuit2_i = Some(circuit_i);
-            }
-            if circuit1_i.is_some() && circuit2_i.is_some() {
-                break;
-            }
-        }
+    let mut circuits: Vec<Vec<usize>> = vec![Vec::with_capacity(10)];
+    circuits[0].push(distances[0].0.0);
+    circuits[0].push(distances[0].0.1);
+    let mut box_to_circuit: HashMap<usize, usize> = HashMap::with_capacity(1000);
+    box_to_circuit.insert(distances[0].0.0, 0);
+    box_to_circuit.insert(distances[0].0.1, 0);
+
+    for &((box1_i, box2_i), distance) in &distances[1..] {
+        // print!("\nBoxes {box1_i} : {box2_i}\t");
+        let circuit1_i = box_to_circuit.get(&box1_i);
+        let circuit2_i = box_to_circuit.get(&box2_i);
 
         match (circuit1_i, circuit2_i) {
             (None, None) => {
                 // both boxes not part of some circuit
-                let mut new_circuit = HashSet::new();
-                new_circuit.insert(*box1_i);
-                new_circuit.insert(*box2_i);
+                let new_circuit = vec![box1_i, box2_i];
+                // println!("New:\t{:?}", new_circuit);
                 circuits.push(new_circuit);
+                box_to_circuit.insert(box1_i, circuits.len() - 1);
+                box_to_circuit.insert(box2_i, circuits.len() - 1);
             }
-            (Some(circuit1_i), None) => {
+            (Some(&circuit1_i), None) => {
                 // first box already in circuit
-                circuits[circuit1_i].insert(*box2_i);
+                circuits[circuit1_i].push(box2_i);
+                box_to_circuit.insert(box2_i, circuit1_i);
+                // println!("Add second:\t{:?}", circuits[circuit1_i]);
             }
-            (None, Some(circuit2_i)) => {
+            (None, Some(&circuit2_i)) => {
                 // second box already in circuit
-                circuits[circuit2_i].insert(*box1_i);
+                circuits[circuit2_i].push(box1_i);
+                box_to_circuit.insert(box1_i, circuit2_i);
+                // println!("Add first:\t{:?}", circuits[circuit2_i]);
             }
-            (Some(circuit1_i), Some(circuit2_i)) => {
-                // both boxes part of different citcuits
+            (Some(&circuit1_i), Some(&circuit2_i)) if circuit1_i != circuit2_i => {
+                // both boxes part of different circuits
                 // merge circuits
-                let merged_circuit: HashSet<usize> = circuits[circuit1_i]
-                    .iter()
-                    .chain(circuits[circuit2_i].iter())
-                    .copied()
-                    .collect();
-                if circuit1_i > circuit2_i {
-                    circuits.swap_remove(circuit1_i);
-                    circuits.swap_remove(circuit2_i);
-                } else {
-                    circuits.swap_remove(circuit2_i);
-                    circuits.swap_remove(circuit1_i);
+                let mut merged_circuit: Vec<usize> =
+                    Vec::with_capacity(circuits[circuit1_i].len() + circuits[circuit2_i].len());
+                for box1_i in &circuits[circuit1_i] {
+                    box_to_circuit.insert(*box1_i, circuits.len());
+                    merged_circuit.push(*box1_i);
                 }
+                for box2_i in &circuits[circuit2_i] {
+                    box_to_circuit.insert(*box2_i, circuits.len());
+                    merged_circuit.push(*box2_i);
+                }
+                circuits[circuit1_i].clear();
+                circuits[circuit2_i].clear();
+                // println!("Merge:\t{:?}", merged_circuit);
                 circuits.push(merged_circuit);
             }
+            (Some(_), Some(_)) => {
+                // both boxes part of the same circuit
+                // println!("In same circuit:\tskip");
+            }
         }
-
-        if circuits[0].len() == boxes.len() {
-            return (boxes[*box1_i].x * boxes[*box2_i].x) as u64;
+        // println!("Circuits:\t{:?}", circuits);
+        // println!("Map:\t{:?}", box_to_circuit);
+        if circuits[circuits.len() - 1].len() == boxes.len() {
+            return (boxes[box1_i].x * boxes[box2_i].x) as u64;
         }
     }
 
